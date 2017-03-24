6753f3addfbb3f3c672ccf54c585205567b4b2cb
ThornDev <jiemianwang@gmail.com>
2017-03-24 16:29:31 +0800

    Initial Commit

---
+++ Modiv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Modiv
+//
+//  Created by 王绵杰 on 2017/3/24.
+//  Copyright © 2017年 王绵杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Modiv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Modiv
+//
+//  Created by 王绵杰 on 2017/3/24.
+//  Copyright © 2017年 王绵杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
